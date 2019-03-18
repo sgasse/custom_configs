3054af380209bd24a12cc91e41e44585bf9f6825
= <=>
2019-03-18 10:28:11 +0100

    Add system-wide gitignore with script to set it

---
+++ setup_git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git config --global user.name = "sgasse"
+git config --global user.email = "sgasse@users.noreply.github.com"
+git config --global core.excludesfile ~/.custom_configs/.gitignore-system
